011ea7097734326324e75406037c5b6914115a14
LCarminatti <lucas.csantos@sptech.school>
2025-04-13 22:10:41 -0300

    fix navbar

--- src/components/NavbarPanel.jsx
+++ src/components/NavbarPanel.jsx
@@ -1,35 +1,49 @@
+import { useState } from "react";
+import Imagem from "../assets/logo.svg";
 import { Plus } from "lucide-react";
-import imagem from "../assets/logo.svg";
 import UserAvatar from "./UserAvatar";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const NavbarPanel = () => {
-  const nomeUsuario = "João";
+const NavbarHome = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="h-[10vh] bg-[#3970B7] text-white flex items-center justify-evenly flex-row">
-      <div>
-        <img src={imagem} className="h-20 w-auto" />
+    <nav className=" h-[12vh] w-full bg-[#3970B7] border-b-4 border-b-[#FECB0A] text-white px-4 md:px-10 flex items-center justify-between md:justify-center flex-row text-sm">
+      <div className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0 md:w-70 flex md:justify-center">
+        <img src={Imagem} className="h-20 " />
       </div>
 
-      <div className="flex flex-row justify-evenly w-[40%]">
-        <span><h2 className="text-xl font-bold mb-4">Início</h2></span>
-        <span><h2 className="text-xl font-bold mb-4">Agendamentos</h2></span>
-        <span><h2 className="text-xl font-bold mb-4">Calendário</h2></span>
-        <span><h2 className="text-xl font-bold mb-4">Contato</h2></span>
+      <div className="hidden md:flex flex-row justify-evenly w-150">
+        <h2 className="mb-4 font-semibold cursor-pointer">Início</h2>
+
+        <h2 className="mb-4 font-semibold cursor-pointer">Agendamentos</h2>
+
+        <h2 className="mb-4 font-semibold cursor-pointer">Calendário</h2>
+
+        <h2 className="mb-4 font-semibold cursor-pointer">Contato</h2>
       </div>
 
-      <div className="flex flex-row items-center gap-3">
+      <div className="flex flex-row items-center gap-10">
         {/* Botão com ícone */}
-        <button className="flex items-center justify-center gap-2 bg-blue-600 text-white px-3 py-2 text-sm rounded-lg hover:bg-blue-700 transition">
+        <button className="flex items-center justify-center gap-2 bg-blue-600 text-white px-3 py-2 text-sm rounded-lg hover:bg-blue-700 transition border-[0.5px] border-white">
           <Plus size={18} />
           Agendar Nova Aula
         </button>
 
         <UserAvatar name="João tarminatti" />
+      </div>
 
+      <div className="absolute right-4 md:hidden">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <XMarkIcon className="h-8 w-8" />
+          ) : (
+            <Bars3Icon className="h-8 w-8" />
+          )}
+        </button>
       </div>
     </nav>
   );
 };
 
-export default NavbarPanel;
+export default NavbarHome;
--- src/pages/StudentInitialPage.jsx
+++ src/pages/StudentInitialPage.jsx
@@ -5,23 +5,27 @@ const CardPanelItem = ({ title, description, buttonLink }) => {
     "Completar Cadastro": {
       bg: "bg-[#FFF5F5]",
       text: "text-[#FF6D0C]",
-      button: "text-[#FF7200]"
+      button: "text-[#FF7200]",
     },
-    "Agendamentos": {
+    Agendamentos: {
       bg: "bg-[#EFF6FF]",
       text: "text-[#1E40AF]",
-      button: "text-[#2563EB]"
+      button: "text-[#2563EB]",
     },
-    "Calendário": {
+    Calendário: {
       bg: "bg-[#FCFDF0]",
       text: "text-[#A39316]",
-      button: "text-[#A39316]"
-    }
+      button: "text-[#A39316]",
+    },
   };
 
   return (
-    <div className={`w-[17vw] h-[30vh] rounded-md shadow-md p-5 mt-5 border ${styles[title].bg}`}>
-      <h2 className={`text-[20px] mb-2 font-semibold ${styles[title].text}`}>{title}</h2>
+    <div
+      className={`w-[17vw] h-[30vh] rounded-md shadow-md p-5 border ${styles[title].bg}`}
+    >
+      <h2 className={`text-[20px] mb-2 font-semibold ${styles[title].text}`}>
+        {title}
+      </h2>
       <p className="text-sm text-black">{description}</p>
       <span className={`block mt-6 font-bold ${styles[title].button}`}>
         {buttonLink}
@@ -32,9 +36,21 @@ const CardPanelItem = ({ title, description, buttonLink }) => {
 
 const StudentInitial = () => {
   const items = [
-    { title: "Completar Cadastro", description: "Seu cadastro ainda está incompleto.", buttonLink: "Completar cadastro →" },
-    { title: "Agendamentos", description: "Visualize e gerencie suas aulas agendadas.", buttonLink: "Ver agendamentos →" },
-    { title: "Calendário", description: "Visualize suas aulas em um calendário mensal.", buttonLink: "Ver calendário →" }
+    {
+      title: "Completar Cadastro",
+      description: "Seu cadastro ainda está incompleto.",
+      buttonLink: "Completar cadastro →",
+    },
+    {
+      title: "Agendamentos",
+      description: "Visualize e gerencie suas aulas agendadas.",
+      buttonLink: "Ver agendamentos →",
+    },
+    {
+      title: "Calendário",
+      description: "Visualize suas aulas em um calendário mensal.",
+      buttonLink: "Ver calendário →",
+    },
   ];
 
   return (
@@ -43,29 +59,37 @@ const StudentInitial = () => {
         <NavbarPanel />
       </div>
 
-      <div className="flex justify-center items-center w-full h-[100px] text-xs mb-6">
-        <h1 className="text-lg font-semibold text-gray-800">Painel do Aluno</h1>
+      <div className="flex justify-center items-center w-full h-[100px] text-xs mb-12">
+        <h1 className="text-3xl font-semibold text-gray-800">
+          Painel do Aluno
+        </h1>
       </div>
 
-      <div className="bg-white rounded-lg shadow-md p-5 w-[895px] h-[350px]">
-        <h1 className="text-[25px] font-bold text-black mb-5">Bem-vindo(a) ao Painel do Aluno</h1>
-        <span className="text-[15px] text-black mb-2 block">
-          Aqui você pode gerenciar suas aulas, verificar agendamentos e realizar pagamentos.
-        </span>
+      <div className="flex justify-center bg-white rounded-lg shadow-md p-5 w-[895px] h-[400px]">
+        <div className="space-y-8 w-full">
+          <h1 className="text-[25px] font-bold text-black mb-6">
+            Bem-vindo(a) ao Painel do Aluno
+          </h1>
 
-        <div className="flex justify-between gap-5 flex-wrap mt-4">
-          {items.map((item, index) => (
-            <CardPanelItem
-              key={index}
-              title={item.title}
-              description={item.description}
-              buttonLink={item.buttonLink}
-            />
-          ))}
+          <p className="text-[15px] text-black mb-6">
+            Aqui você pode gerenciar suas aulas, verificar agendamentos e realizar pagamentos.
+          </p>
+
+          <div className="flex justify-between flex-wrap gap-5 mb-6">
+            {items.map((item, index) => (
+              <CardPanelItem
+                key={index}
+                title={item.title}
+                description={item.description}
+                buttonLink={item.buttonLink}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default StudentInitial;
