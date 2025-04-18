1d63d17f9edb4674352e148fd2a08edd29f1602a
Matheusbarros54 <matheus.barros@sptech.school>
2025-04-17 21:09:49 -0300

    feat: logo in sidebar dashboard

--- src/components/dashboard-admin/Sidebar.jsx
+++ src/components/dashboard-admin/Sidebar.jsx
@@ -7,25 +7,26 @@ import {
     CreditCard,
     FileText,
     Settings
-  } from "lucide-react";
-  import { SidebarNavItem } from "./SidebarNavItem";
-  
-  export function Sidebar() {
+} from "lucide-react";
+import { SidebarNavItem } from "./SidebarNavItem";
+import logo from "../../assets/logo.svg";
+
+export function Sidebar() {
     return (
-      <aside className="w-64 h-screen fixed top-0 left-0 bg-[#3970B7] py-4 px-2 space-y-2 z-50">
-        <div className="px-4 pb-6">
-          <span className="text-white font-extrabold text-xl tracking-wide">STUDI</span>
-        </div>
-  
-        <SidebarNavItem label="Visão Geral" icon={<Home size={18} />} to="/dashboard" />
-        <SidebarNavItem label="Finanças" icon={<BarChart2 size={18} />} to="/dashboard/financas" />
-        <SidebarNavItem label="Professores" icon={<Users size={18} />} to="/dashboard/professores" />
-        <SidebarNavItem label="Agendamentos" icon={<CalendarDays size={18} />} to="/dashboard/agendamentos" />
-        <SidebarNavItem label="Pagamentos" icon={<CreditCard size={18} />} to="/dashboard/pagamentos" />
-        <SidebarNavItem label="Relatórios" icon={<FileText size={18} />} to="/dashboard/relatorios" />
-        <SidebarNavItem label="Configurações" icon={<Settings size={18} />} to="/dashboard/configuracoes" />
-  
-      </aside>
+        <aside className="w-64 h-screen fixed top-0 left-0 bg-[#3970B7] py-4 px-2 space-y-2 z-50">
+            <div className="px-4 pb-6">
+                <img src={logo} alt="Logo" className="h-16 w-auto" />
+
+            </div>
+
+            <SidebarNavItem label="Visão Geral" icon={<Home size={18} />} to="/dashboard" />
+            <SidebarNavItem label="Finanças" icon={<BarChart2 size={18} />} to="/dashboard/financas" />
+            <SidebarNavItem label="Professores" icon={<Users size={18} />} to="/dashboard/professores" />
+            <SidebarNavItem label="Agendamentos" icon={<CalendarDays size={18} />} to="/dashboard/agendamentos" />
+            <SidebarNavItem label="Pagamentos" icon={<CreditCard size={18} />} to="/dashboard/pagamentos" />
+            <SidebarNavItem label="Relatórios" icon={<FileText size={18} />} to="/dashboard/relatorios" />
+            <SidebarNavItem label="Configurações" icon={<Settings size={18} />} to="/dashboard/configuracoes" />
+
+        </aside>
     );
-  }
-  
+}
