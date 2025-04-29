0b543b216d6032ecc08655c38a81c47860b1b1fb
LCarminatti <lucas.csantos@sptech.school>
2025-04-29 17:52:03 -0300

    integrate login and register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -6,105 +6,177 @@ import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 import { studentService } from '../services/studentService';
 import LoadingButton from '../components/ui/LoadingButton';
 import { showAlert } from '../components/ShowAlert';
-import { FcGoogle } from "react-icons/fc";
-
 
 const RegisterPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    return (
-        <div className="flex flex-col h-screen w-screen">
-            <NavbarHome />
-            <main className='h-[88vh] w-screen bg-no-repeat bg-cover bg-center flex justify-center items-center' style={{ backgroundImage: `url(${Imagem})` }}>
+    if (formData.password !== formData.confirmPassword) {
+      showAlert({
+        title: 'Erro!',
+        text: "As senhas não coincidem",
+        icon: 'error'
+      });
+      setLoading(false);
+      return;
+    }
 
-                <section className='h-full w-full md:h-140 md:w-120 bg-[#3970B7] py-4 md:border-4 md:border-[#FECB0A] md:rounded-3xl text-white'>
+    try {
+      const { name, email, password } = formData;
+      const response = await studentService.create({ name, email, password });
 
-                    <form className='flex flex-col justify-evenly items-center w-full max-w-screen-xl mx-auto h-full'>
+      showAlert({
+        title: 'Conta criada!',
+        text: `Bem-vindo, ${response.username || response.name || 'usuário'}! 🎉`,
+        icon: 'success'
+      });
 
-                        <h1 className='text-[25px] font-bold'>Crie uma conta</h1>
+      setTimeout(() => {
+        navigate('/entrar');
+      }, 2000);
+    } catch (err) {
+      console.error("Erro ao tentar cadastrar:", err);
+      showAlert({
+        title: 'Erro!',
+        text: "Erro ao realizar cadastro. Tente novamente",
+        icon: 'error'
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
-                        <label className='flex flex-col w-72 md:w-80 gap-1'>
-                            <span className='font-bold text-xs'>Nome Completo</span>
-                            <input className='rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 text-black text-xs pl-3' type="text" placeholder='Digite seu nome completo' />
-                        </label>
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <NavbarHome />
 
-                        <label className='flex flex-col w-72 md:w-80 gap-1'>
-                            <span className='font-bold text-xs'>E-mail</span>
-                            <input className='rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 text-black text-xs pl-3' type='email' placeholder='seu@email.com' />
-                        </label>
+      <main
+        className="h-[88vh] w-screen bg-no-repeat bg-cover bg-center flex justify-center items-center"
+        style={{ backgroundImage: `url(${Imagem})` }}
+      >
+        <section className="w-[90%] max-w-[400px] bg-[#3970B7] py-6 px-4 md:border-4 md:border-[#FECB0A] md:rounded-3xl text-white flex flex-col items-center">
+          <form
+            className="flex flex-col items-center h-full w-full md:h-full md:w-full gap-6"
+            onSubmit={handleRegister}
+          >
+            <h1 className="text-[25px] font-bold mb-2">Crie uma conta</h1>
 
-                        <label className='flex flex-col w-72 md:w-80 gap-1'>
-                            <span className='font-bold text-xs'>Senha</span>
-                            <div className="relative">
-                                <input
-                                    className='w-full rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 px-3 pr-10 text-black text-xs pl-3'
-                                    type={showPassword ? "text" : "password"}
-                                    placeholder='Crie uma senha'
-                                />
-                                <button
-                                    type="button"
-                                    className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-700"
-                                    onClick={() => setShowPassword(!showPassword)}
-                                >
-                                    {showPassword ? <EyeSlashIcon className="hover:cursor-pointer w-5 h-5" /> : <EyeIcon className="hover:cursor-pointer w-5 h-5" />}
-                                </button>
-                            </div>
-                        </label>
+            <label className="flex flex-col w-75 md:w-80 gap-1">
+              <span className="font-bold text-xs">Nome Completo</span>
+              <input
+                className="rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 text-black text-xs pl-3"
+                type="text"
+                placeholder="Digite seu nome completo"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
+            </label>
 
-                        <label className='flex flex-col w-72 md:w-80 gap-3'>
-                            <div className='flex flex-col gap-1'>
-                                <span className='font-bold text-xs'>Confirmar Senha</span>
-                                <div className="relative">
-                                    <input
-                                        className='w-full rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 px-4 pr-10 text-black text-xs pl-3'
-                                        type={showConfirmPassword ? "text" : "password"}
-                                        placeholder='Confirme sua senha'
-                                    />
-                                    <button
-                                        type="button"
-                                        className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-700"
-                                        onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                                    >
-                                        {showConfirmPassword ? <EyeSlashIcon className="hover:cursor-pointer w-5 h-5" /> : <EyeIcon className="hover:cursor-pointer w-5 h-5" />}
-                                    </button>
-                                </div>
-                            </div>
-                            <div className='flex gap-2'>
-                                <input className='hover:cursor-pointer' type="checkbox" />
-                                <span className='font-bold text-xs'>
-                                    I agree the <a className='text-[#FECB0A] hover:underline' href="">Terms of Service</a> and <a className='text-[#FECB0A] hover:underline' href="">Privacy Policy</a>
-                                </span>
-                            </div>
-                        </label>
+            <label className="flex flex-col w-75 md:w-80 gap-1">
+              <span className="font-bold text-xs">E-mail</span>
+              <input
+                className="rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 text-black text-xs pl-3"
+                type="email"
+                placeholder="seu@email.com"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+            </label>
 
+            <label className="flex flex-col w-75 md:w-80 gap-1">
+              <span className="font-bold text-xs">Senha</span>
+              <div className="relative">
+                <input
+                  className="w-full rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 px-3 pr-10 text-black text-xs pl-3"
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Crie uma senha"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-700"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeSlashIcon className="hover:cursor-pointer w-5 h-5" />
+                  ) : (
+                    <EyeIcon className="hover:cursor-pointer w-5 h-5" />
+                  )}
+                </button>
+              </div>
+              <small className="text-xs text-white mt-1">A senha deve ter pelo menos 6 caracteres.</small>
+            </label>
 
+            <label className="flex flex-col w-75 md:w-80 gap-1">
+              <span className="font-bold text-xs">Confirmar Senha</span>
+              <div className="relative">
+                <input
+                  className="w-full rounded-md bg-white placeholder-[#64748B] placeholder:text-xs h-8 px-3 pr-10 text-black text-xs pl-3"
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  placeholder="Confirme sua senha"
+                  value={formData.confirmPassword}
+                  onChange={(e) =>
+                    setFormData({ ...formData, confirmPassword: e.target.value })
+                  }
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-700"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  {showConfirmPassword ? (
+                    <EyeSlashIcon className="hover:cursor-pointer w-5 h-5" />
+                  ) : (
+                    <EyeIcon className="hover:cursor-pointer w-5 h-5" />
+                  )}
+                </button>
+              </div>
+            </label>
 
-                        <button className='rounded-lg bg-[#FECB0A] text-black font-semibold cursor-pointer w-72 md:w-80 h-10 text-sm'>Cadastrar</button>
+            <LoadingButton
+              isLoading={loading}
+              type="submit"
+              className="rounded-lg bg-[#FECB0A] text-black font-semibold cursor-pointer w-75 md:w-80 h-10 text-sm mt-2"
+            >
+              Cadastrar
+            </LoadingButton>
 
-                        <div className="h-2 flex items-center my-6">
+            {/* Botão de login */}
+            <p className="text-sm mt-2">
+              Já tem uma conta?{' '}
+              <button
+                type="button"
+                onClick={() => navigate('/entrar')}
+                className="font-bold text-[#FECB0A] hover:underline cursor-pointer"
+              >
+                Clique aqui
+              </button>
+            </p>
 
-                            <div className="flex-1 border w-65 md:w-72 border-white"></div>
-
-                            <div className="text-[#64748B] text-center w-8 h-6 bg-white mx-2 ">OU</div>
-
-                            <div className="flex-1 border border-white"></div>
-
-                        </div>
-
-                        <button className='flex items-center justify-center gap-2 rounded-lg bg-white text-black font-normal cursor-pointer w-72 md:w-80 h-10 text-sm'><FcGoogle className="text-2xl" />Cadastre-se pelo Google</button>
-
-                        <span className='text-xs font-bold'>Já tem uma conta aqui? <a className='text-[#FECB0A] hover:underline' href="">Clique aqui</a></span>
-
-
-                    </form>
-
-                </section>
-
-            </main>
-        </div>
-    );
+          </form>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default RegisterPage;
