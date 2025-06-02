1a8f9b161f1b23a916370f7c21f817e3d4efe622
Matheusbarros54 <matheus.barros@sptech.school>
2025-06-01 23:28:40 -0300

    fix: size of text in appointment manager

--- src/pages/AppointmentManager.jsx
+++ src/pages/AppointmentManager.jsx
@@ -55,7 +55,7 @@ export const AppointmentManager = () => {
     <div className="w-full min-h-screen bg-gray-50">
       <NavbarPanel />
       <main className="w-5/6 mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-8">
+        <h1 className="text-2xl font-bold text-center mb-3">
           Acompanhe seus agendamentos
         </h1>
 
