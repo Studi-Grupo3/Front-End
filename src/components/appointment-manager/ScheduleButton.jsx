7cb9a8fd5cca3cfc5fdb710839735fdbc18a7e26
LCarminatti <lucas.csantos@sptech.school>
2025-08-14 20:19:24 -0300

    add hook in ScheduleButton

--- src/components/appointment-manager/ScheduleButton.jsx
+++ src/components/appointment-manager/ScheduleButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { CalendarPlus } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
-export const ScheduleButton = ({ onClick }) => {
+export const ScheduleButton = () => {
+  const navigate = useNavigate();
+
   return (
     <button
-      onClick={onClick}
+      onClick={() => navigate("/aluno/agendamento/criar")}
       className="bg-[#2462C2] border-[0.1px] border-white cursor-pointer hover:bg-blue-500 text-white flex items-center gap-2 px-3 py-2 rounded-md font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
     >
       <CalendarPlus size={18} />
